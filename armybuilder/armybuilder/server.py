fa24c4ad4bea71a8a4b2db066c81cbe5dd1f0980
AcidBrother <52380857+AcidBrother@users.noreply.github.com>
2021-12-30 20:01:30 +0300

    Add files via upload

---
+++ armybuilder/armybuilder/server.py
@@ -0,0 +1,46 @@
+from __future__ import print_function
+from pprint import pprint
+from flask import Flask
+from flask import render_template
+from project import *
+import json
+import sqlite3
+#export FLASK_APP=server
+
+app = Flask(__name__)
+
+print("")
+print("⣿⣿⣿⣿⣿⣿⠏⠌⣾⣿⣿")
+print("⣿⣿⣿⣿⣿⠀⠀⠸⠿⣿⣿⣿")
+print("⣿⣿⣿⣿⠃⠀⣠⣾⣿⣿⣿")
+print("⣿⣿⡿⠃⠀⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿")
+print("⣿⣿⠃⠀⠀⣾⣿⣿⣿⣿⣿⣦⠀⠈⠻⣿⣿⣿")
+print("⣿⣿⠀⠀⠀⣿⣿⣿⠟⠉⠉⠉⢃⣤⠀⠈⢿⣿⣿⣿")
+print("⣿⣿⠀⠀⠀⢸⣿⡟⠀⠀⠀⠀⢹⣿⣧⠀⠀⠙⣿⣿")
+print("⣿⣿⡆⠀⠀⠈⠻⡅⠀⠀⠀⠀⣸⣿⠿⠇⠀⠀⢸⣿")
+print("⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠔⠛⠁⠀⠀⠀⣠⣿⣿")
+print("⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿")
+print("⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿")
+print("⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⣠⣿")
+print("⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⢰⣿")
+print("⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿")
+print("⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿")
+print("⣿⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿")
+print("⣿⣿⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿")
+print("⣿⡟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿")
+print("")
+
+@app.route("/")
+def list_units():
+    return render_template('list_units.html')
+
+@app.route('/necrons/')
+def ()
+
+@app.route('/unit/')
+@app.route('/unit/<name>')
+def unit(name=None):
+    if (name=="necw"):
+        return render_template('unit.html',n=n1)
+    if (name=="cads"):
+        return render_template('unit.html',n=n2)
